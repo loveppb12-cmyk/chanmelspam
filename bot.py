16c14bce4dffa7b3dbfce87f370f72fc446c8a77
loveppb12-cmyk <loveppb12@gmail.com>
2026-05-01 17:53:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram import Bot, Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 from telegram.error import TelegramError
 
-TOKEN = os.environ.get("BOT_TOKEN", "YOUR_TOKEN_HERE")
+TOKEN = os.environ.get("8688313238:AAF8-e_9RlUAq9cZy70xkITUCCG6WyqCsTQ", "YOUR_TOKEN_HERE")
 OWNER_ID = 8595518118  # Your owner ID
 
 # Store active channels and their settings
